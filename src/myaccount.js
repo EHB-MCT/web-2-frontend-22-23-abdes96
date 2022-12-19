e2e18fa915146e272453800816862fd13ddacba9
Abdeslam Boutaarourt <abdeslam.boutaarourt@student.@ehb.be>
2022-12-19 23:36:05 +0100

    profilepage

---
+++ src/myaccount.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+
+    document.getElementById("deleteprofile").addEventListener("click", ()=> {
+    document.getElementById("confirm").style.display = "block";
+    console.log('test');
+})
+
+
+
+}
